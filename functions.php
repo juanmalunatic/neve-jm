0b35ad89750d4374d796dccf7049d68fb8d2e502
Juan Manuel Luna López <lunalopezjm@gmail.com>
2020-06-04 12:48:14 -0500

    Widget structure for custom post type Projects

--- functions.php
+++ functions.php
@@ -12,3 +12,35 @@ if ( ! function_exists( 'neve_child_load_css' ) ):
 	}
 endif;
 add_action( 'wp_enqueue_scripts', 'neve_child_load_css', 20 );
+
+
+// Custom Widgets. TO-DO convert to plugins.
+class widget__cpt_projects extends WP_Widget
+{
+
+	function __construct()
+	{
+		parent::__construct(
+			// Base ID of your widget
+			'jm_widget__cpt_projects',
+			// Widget name that will appear in UI
+			'Projects ordered by date (CPT)',
+			// Other data
+			[
+				'description' => 'Show latest projects.',
+			]
+		);
+	}
+
+	public function widget($args, $instance)
+	{
+		echo "The widget works!";
+	}
+}
+
+// Register all custom widgets
+function jm_load_widgets()
+{
+	register_widget('jm_widget__cpt_projects');
+}
+add_action('widgets_init', 'jm_load_widgets');
