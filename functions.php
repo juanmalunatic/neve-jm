865edcb96932583267af7839db994df69d103284
Juan Manuel Luna López <lunalopezjm@gmail.com>
2020-06-04 20:41:41 -0500

    Project: Metadata PHP + CSS

--- functions.php
+++ functions.php
@@ -130,3 +130,108 @@ function jm_load_widgets()
 	register_widget('jm_widget__cpt_projects');
 }
 add_action('widgets_init', 'jm_load_widgets');
+
+
+// -----------------------------------------------------------
+// Custom function hooks to add ACF data to Project types
+// -----------------------------------------------------------
+
+
+function project_metadata_render () {
+	global $post;
+
+	// Only apply to Project type
+	if ($post->post_type === "project") {
+
+		// Get data
+		$postID = $post->ID;
+		$pairs = [
+			"Link:"     => project_metadata_get_external_link($postID),
+			"Tech:"     => project_metadata_get_tech_links($postID),
+			"Date:" => project_metadata_get_released($postID),
+		];
+
+		// Populate template
+		$pairsHtml = project_metadata_fill_tpl($pairs);
+
+		echo <<< HTML
+			<div id="project-metadata">
+				$pairsHtml
+			</div>
+		HTML;
+
+	};
+}
+
+function project_metadata_fill_tpl ($pairs) {
+	$output = "";
+	// Iterate and switch based on data type
+	foreach ($pairs as $label => $value) {
+		// If value is empty, skip the field
+		if (empty($value)) continue;
+
+		if (is_array($value)) {
+			// Right now, we're only getting an array if it's Tech
+			$content = project_metadata_tpl_tech($value);
+		} else {
+			$content = $value;
+		}
+		$output .= project_metadata_tpl_pair($label, $content);
+	}
+	return $output;
+}
+
+function project_metadata_tpl_pair ($label, $value) {
+	return <<<HTML
+		<!--div class="pair"-->
+			<div class="pair-label">
+				$label
+			</div>
+			<div class="pair-value">
+				$value
+			</div>
+		<!--/div-->
+	HTML;
+}
+
+function project_metadata_tpl_tech ($values) {
+	$tpl = [];
+	foreach ($values as $tech) {
+		$name = $tech['name'];
+		$link = $tech['link'];
+		$tpl []= <<<HTML
+			<a href="$link"> $name </a>
+		HTML;
+	}
+	return implode(", ", $tpl);
+}
+
+function project_metadata_get_tech_links ($postID) {
+	$terms = wp_get_object_terms($postID, 'tech', ['orderby' => 'term_order']);
+	$data = [];
+	/** @var WP_Term $term  */
+	foreach ($terms as $term) {
+		$term_data = [
+			"name" => $term->name,
+			"link" => get_term_link($term->term_id, 'tech'),
+		];
+
+		$data[]= $term_data;
+	}
+	return $data;
+}
+
+function project_metadata_get_external_link ($postID) {
+	$link = get_post_meta($postID, 'external_link')[0];
+	return <<<HTML
+		<a href="$link">$link</a>
+	HTML;
+}
+
+function project_metadata_get_released($postID) {
+	$raw = get_post_meta($postID, 'released')[0];
+	$date = DateTime::createFromFormat('Ymd', $raw);
+	return $date->format('F Y');
+}
+
+add_action('neve_before_content', 'project_metadata_render');
